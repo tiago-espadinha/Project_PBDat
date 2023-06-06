4caba863a2baa72f27c3fbdf915b339563343853
DanielPaulo <danipaulo2001@gmail.com>
2023-06-06 10:29:33 +0100

    feat: add missing values algorithm and skeleton printing

--- PBDat_Giro_Proj.py
+++ PBDat_Giro_Proj.py
@@ -8,6 +8,195 @@ from sklearn.decomposition import PCA
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 
+
+def print_skel(i,skel_data,frame,width,height): #prints skel onto video, prob_val is calculated experimentaly
+    val=4
+    prob_val=0.20
+    #0-1
+    if skel_data[6,i]>prob_val and skel_data[3,i]>prob_val:
+        cv2.line(img=frame, pt1=(int(skel_data[1,i]*width),int(skel_data[2,i]*height)), pt2=(int(skel_data[4,i]*width),int(skel_data[5,i]*height)), color=(255, 0, 0), thickness=val , lineType=8, shift=0)
+    #1-2
+    if skel_data[9,i]>prob_val and skel_data[6,i]>prob_val:
+        cv2.line(img=frame, pt1=(int(skel_data[4,i]*width),int(skel_data[5,i]*height)), pt2=(int(skel_data[7,i]*width),int(skel_data[8,i]*height)), color=(255, 0, 0), thickness=val , lineType=8, shift=0)
+    #2-3
+    if skel_data[12,i]>prob_val and skel_data[9,i]>prob_val:
+        cv2.line(img=frame, pt1=(int(skel_data[7,i]*width),int(skel_data[8,i]*height)), pt2=(int(skel_data[10,i]*width),int(skel_data[11,i]*height)), color=(255, 0, 0), thickness=val , lineType=8, shift=0)
+    #3-4
+    if skel_data[15,i]>prob_val and skel_data[12,i]>prob_val:
+        cv2.line(img=frame, pt1=(int(skel_data[10,i]*width),int(skel_data[11,i]*height)), pt2=(int(skel_data[13,i]*width),int(skel_data[14,i]*height)), color=(255, 0, 0), thickness=val , lineType=8, shift=0)
+    #1-5
+    if skel_data[18,i]>prob_val and skel_data[6,i]>prob_val:
+        cv2.line(img=frame, pt1=(int(skel_data[4,i]*width),int(skel_data[5,i]*height)), pt2=(int(skel_data[16,i]*width),int(skel_data[17,i]*height)), color=(255, 0, 0), thickness=val , lineType=8, shift=0)
+    #5-6
+    if skel_data[21,i]>prob_val and skel_data[18,i]>prob_val:
+        cv2.line(img=frame, pt1=(int(skel_data[16,i]*width),int(skel_data[17,i]*height)), pt2=(int(skel_data[19,i]*width),int(skel_data[20,i]*height)), color=(255, 0, 0), thickness=val , lineType=8, shift=0)
+    #6-7
+    if skel_data[24,i]>prob_val and skel_data[21,i]>prob_val:
+        cv2.line(img=frame, pt1=(int(skel_data[19,i]*width),int(skel_data[20,i]*height)), pt2=(int(skel_data[22,i]*width),int(skel_data[23,i]*height)), color=(255, 0, 0), thickness=val , lineType=8, shift=0)
+    #1-8
+    if skel_data[27,i]>prob_val and skel_data[6,i]>prob_val:
+        cv2.line(img=frame, pt1=(int(skel_data[4,i]*width),int(skel_data[5,i]*height)), pt2=(int(skel_data[25,i]*width),int(skel_data[26,i]*height)), color=(255, 0, 0), thickness=val , lineType=8, shift=0)
+    #8-9
+    if skel_data[30,i]>prob_val and skel_data[27,i]>prob_val:
+        cv2.line(img=frame, pt1=(int(skel_data[25,i]*width),int(skel_data[26,i]*height)), pt2=(int(skel_data[28,i]*width),int(skel_data[29,i]*height)), color=(255, 0, 0), thickness=val , lineType=8, shift=0)
+    #9-10
+    if skel_data[33,i]>prob_val and skel_data[30,i]>prob_val:
+        cv2.line(img=frame, pt1=(int(skel_data[28,i]*width),int(skel_data[29,i]*height)), pt2=(int(skel_data[31,i]*width),int(skel_data[32,i]*height)), color=(255, 0, 0), thickness=val , lineType=8, shift=0)
+    #1-11
+    if skel_data[36,i]>prob_val and skel_data[6,i]>prob_val:
+        cv2.line(img=frame, pt1=(int(skel_data[4,i]*width),int(skel_data[5,i]*height)), pt2=(int(skel_data[34,i]*width),int(skel_data[35,i]*height)), color=(255, 0, 0), thickness=val , lineType=8, shift=0)
+    #11-12
+    if skel_data[39,i]>prob_val and skel_data[36,i]>prob_val:
+        cv2.line(img=frame, pt1=(int(skel_data[34,i]*width),int(skel_data[35,i]*height)), pt2=(int(skel_data[37,i]*width),int(skel_data[38,i]*height)), color=(255, 0, 0), thickness=val , lineType=8, shift=0)
+    #12-13
+    if skel_data[42,i]>prob_val and skel_data[39,i]>prob_val:
+        cv2.line(img=frame, pt1=(int(skel_data[37,i]*width),int(skel_data[38,i]*height)), pt2=(int(skel_data[40,i]*width),int(skel_data[41,i]*height)), color=(255, 0, 0), thickness=val , lineType=8, shift=0)
+    #0-14
+    if skel_data[45,i]>prob_val and skel_data[3,i]>prob_val:
+        cv2.line(img=frame, pt1=(int(skel_data[1,i]*width),int(skel_data[2,i]*height)), pt2=(int(skel_data[43,i]*width),int(skel_data[44,i]*height)), color=(255, 0, 0), thickness=val , lineType=8, shift=0)
+    #14-16
+    if skel_data[51,i]>prob_val and skel_data[45,i]>prob_val:
+        cv2.line(img=frame, pt1=(int(skel_data[43,i]*width),int(skel_data[44,i]*height)), pt2=(int(skel_data[49,i]*width),int(skel_data[50,i]*height)), color=(255, 0, 0), thickness=val , lineType=8, shift=0)
+    #0-15
+    if skel_data[48,i]>prob_val and skel_data[3,i]>prob_val:
+        cv2.line(img=frame, pt1=(int(skel_data[1,i]*width),int(skel_data[2,i]*height)), pt2=(int(skel_data[46,i]*width),int(skel_data[47,i]*height)), color=(255, 0, 0), thickness=val , lineType=8, shift=0)
+    #15-17
+    if skel_data[54,i]>prob_val and skel_data[48,i]>prob_val:
+        cv2.line(img=frame, pt1=(int(skel_data[46,i]*width),int(skel_data[47,i]*height)), pt2=(int(skel_data[52,i]*width),int(skel_data[53,i]*height)), color=(255, 0, 0), thickness=val , lineType=8, shift=0)
+
+def fill_missing(skel_data):
+    #Desc:
+    #   fill missing values with the mean
+    #Inputs:
+    #   skel_data->data matrix
+    #Outputs:
+    #   missing_index->index of missing values from incomplete matrix 
+    #   n->nº of missing values
+    n_row=skel_data[:,0].shape
+    n_col=skel_data[0,:].shape
+    missing_index=np.zeros((n_row[0],n_col[0]))
+    for i in range(n_row[0]):
+        sum=0
+        n=0
+        for j in range(n_col[0]):
+            if skel_data[i,j]==0:
+                missing_index[i][j]=1
+            else:
+                sum+=skel_data[i,j]
+                n+=1
+        for j in range(n_col[0]):
+            if missing_index[i][j]==1:
+
+                if n!=0:
+                    skel_data[i,j]=0
+    return missing_index,n
+
+def fill_missing_alg(skel,OG_skel,r,alfa): #matrix completion algoritm
+    #Desc:
+    #   This will use SVD to complete a matrix with missing values. To do this it will also center and normalize the datamatrix
+    #Inputs:
+    #   skel->matrix to be completed
+    #   OG_skel->matrix thats already completed for comparation
+    #   r-> rank of matrix, can be higher or equal, the closest the better
+    #   alfa-> error for which we consider that it has already converged to a satisfactory solution
+    #Outputs:
+    #   skel->completed matrix
+    #   missing_index->index of missing values from original incomplete matrix
+    #   it-> nº of iterations done to acomplish result
+
+    original_data=np.copy(skel)
+    skel=np.delete(skel,[0,3,6,9,12,15,18,21,24,27,30,33,36,39,42,45,48,51,54],0) #we take off index and probabilities
+    OG_skel=np.delete(OG_skel,[0,3,6,9,12,15,18,21,24,27,30,33,36,39,42,45,48,51,54],0) #we take off index and probabilities
+    missing_index,n=fill_missing(skel) #get missing indexes and fill in with something (can be mean, or just some random number)
+    alfa=alfa/n
+    err_dif=10000000000000000000000000;
+    last_error=1000;
+    it=0
+    old_skel=np.copy(skel)
+    while err_dif>alfa and it<200 : # this will stop at either some iteration or when the SVD stabalizes
+        norm_val_x=np.zeros(skel[0,:].shape[0])
+        norm_val_y=np.zeros(skel[0,:].shape[0])
+        sub_val_x=np.zeros(skel[0,:].shape[0])
+        sub_val_y=np.zeros(skel[0,:].shape[0])
+        for i in range(skel[0,:].shape[0]): #get maximum value
+            for j in range(skel[:,0].shape[0]):
+                if j%2==0:
+                    if skel[j,i]>norm_val_x[i]:
+                        norm_val_x[i]=np.copy(skel[j,i])
+                else:
+                    if skel[j,i]>norm_val_y[i]:
+                        norm_val_y[i]=np.copy(skel[j,i])
+        #normalize and centralize data
+
+        for i in range(skel[0,:].shape[0]):
+            for j in range(skel[:,0].shape[0]):
+                if j%2==0:
+                    skel[j,i]=skel[j,i]/norm_val_x[i]
+                    if j==0:
+                        sub_val_x[i]=skel[j,i]
+                    skel[j,i]=skel[j,i]-sub_val_x[i]
+                else:
+                    skel[j,i]=skel[j,i]/norm_val_y[i]
+                    if j==1:
+                        sub_val_y[i]=skel[j,i]
+                    skel[j,i]=skel[j,i]-sub_val_y[i]
+        #get prediction
+
+        u, s, v = np.linalg.svd(skel, full_matrices=False) #SVD
+        s=np.diag(s)
+        skel_pred =  u[:, :r] @ s[0:r, 0:r] @ v[0:r, :] #reconstruct
+        for i in range(skel[0,:].shape[0]):
+            for j in range(skel[:,0].shape[0]):
+                if missing_index[j,i]==1:
+                    skel[j,i]=np.copy(skel_pred[j,i]) 
+        #return to its place in the video
+
+        for i in range(skel[0,:].shape[0]):
+            for j in range(skel[:,0].shape[0]):
+                if j%2==0:
+                    skel[j,i]=skel[j,i]+sub_val_x[i]
+                    skel[j,i]=skel[j,i]*norm_val_x[i]
+                else:
+                    skel[j,i]=skel[j,i]+sub_val_y[i]
+                    skel[j,i]=skel[j,i]*norm_val_y[i] 
+        #calculate error between iterations, this isint to show that its getting "better", but to show that its converging
+
+        err=0
+        n_err=0
+        for i in range(skel[0,:].shape[0]): #get error, which is the distance of each predicted point to its previous predictions
+            for j in range(skel[:,0].shape[0]):
+                if missing_index[j,i]==1 and j%2==0:
+                    aux=float((old_skel[j,i]-skel[j,i])*(old_skel[j+1,i]-skel[j+1,i]))
+                    err+=math.sqrt(aux*aux)
+                    n_err+=1
+        final_error=err/n_err #we will use the normalized error because its easier to analize
+        err_dif=last_error-final_error
+        if err_dif<0: #neglect negative errors
+            err_dif=100000000000000000
+        last_error=final_error
+        old_skel=np.copy(skel)
+        it+=1
+        print("Did iteration: ",it)
+        print("error: ",final_error)   
+    #since we have the complete skelectons, we can also have an "absolute" error
+
+    err=0
+    n_err=0
+    for i in range(skel[0,:].shape[0]): #get error, no real meaning, only for comparations sake
+        for j in range(skel[:,0].shape[0]):
+            if missing_index[j,i]==1 and j%2==0:
+                aux=float((OG_skel[j,i]-skel[j,i])*(OG_skel[j+1,i]-skel[j+1,i]))
+                err+=math.sqrt(aux*aux)
+                n_err+=1
+    final_error=err/n_err
+    print("Error relative to the complete skelectons: ",final_error)
+    for i in range(18): #return to its normal shape
+        original_data[(i*3)+1,:]=np.copy(skel[(i*2),:])
+        original_data[(i*3)+2,:]=np.copy(skel[(i*2)+1,:])
+    skel=np.copy(original_data)
+    return skel,missing_index,it
+
+
 # Print flags
 print_plots = True
 print_frames = True
@@ -24,6 +213,12 @@ var = 0.85
 vid = cv2.VideoCapture('data/girosmallveryslow2.mp4')
 n_frames = int(vid.get(cv2.CAP_PROP_FRAME_COUNT))
 
+#skelecton init
+skel=scipy.io.loadmat('data/girosmallveryslow2_openpose.mat')
+skel=skel['skeldata']
+skel_comp=scipy.io.loadmat('data/girosmallveryslow2_openpose_complete.mat')
+skel_comp=skel_comp['skeldata']
+
 # Loading embeddings
 data = scipy.io.loadmat('data/girosmallveryslow2.mp4_features.mat')
 data = data['features'] 
@@ -32,6 +227,10 @@ data = data['features']
 subset = data[:, frame_start:frame_start + frame_count] # select a subset of 100 frames
 subset = subset.T
 
+#Fill missing data
+
+skel,missing_index,it=fill_missing_alg(skel,skel_comp,4,0.001) 
+print("This took ",it," iterations")
 
 # PCA Manual
 
